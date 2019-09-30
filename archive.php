23a63a287a5a4101e6a94f8404f68bf403c6a323
FlyingSky <me@fsky7.com>
2019-09-30 19:55:36 +0800

    UI | 面包屑样式更改

--- archive.php
+++ archive.php
@@ -1,13 +1,18 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 <div id="main">
-<div style="margin-top:24px;"><a href="<?php $this->options->siteUrl(); ?>">首页</a> &raquo; <?php $this->archiveTitle(array(
-'category'  =>  _t('分类 %s 下的文章'),
-'search'    =>  _t('包含关键字 %s 的文章'),
-'tag'       =>  _t('标签 %s 下的文章'),
-'date'      =>  _t('在 %s 发布的文章'),
-'author'    =>  _t('作者 %s 发布的文章')
-), '', ''); ?></div>
+<div class="mdui-card" style="margin-top:20px;background-color: rgba(180, 180, 180, 0.25);">
+    <span class="mdui-chip-icon" style="border-radius:2px;"><i class="mdui-icon material-icons">chevron_right</i></span>
+    <span class="mdui-chip-title">
+        <a href="<?php $this->options->siteUrl(); ?>">首页</a> &raquo; <?php $this->archiveTitle(array(
+        'category'  =>  _t('分类 %s 下的文章'),
+        'search'    =>  _t('包含关键字 %s 的文章'),
+        'tag'       =>  _t('标签 %s 下的文章'),
+        'date'      =>  _t('在 %s 发布的文章'),
+        'author'    =>  _t('作者 %s 发布的文章')
+        ), '', ''); ?>
+    </span>
+</div>
 <?php if ($this->have()): ?>
 <?php while($this->next()): ?>
 <div class="mdui-card<?php if ($this->options->PjaxOption && $this->hidden): ?> protected<?php endif; ?>" style="margin-top: 20px;">
