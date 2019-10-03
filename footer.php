1987dbebdb578992ddf2c3dab16176decab16819
paizi <admin@blingwang.cn>
2019-10-03 23:02:01 +0800

    添加ICP备案指向链接
    Signed-off-by: paizi <admin@blingwang.cn>

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
                 <script>function show_runtime(){window.setTimeout("show_runtime()",1000);X=new Date("<?=$this->options->SiteTime?>");Y=new Date();T=(Y.getTime()-X.getTime());M=24*60*60*1000;a=T/M;A=Math.floor(a);b=(a-A)*24;B=Math.floor(b);c=(b-B)*60;C=Math.floor((b-B)*60);D=Math.floor((c-C)*60);runtime_span.innerHTML=""+A+" 天 "+B+" 时 "+C+" 分 "+D+" 秒"}show_runtime();</script>
                 <?php endif; ?>
                 <?php if ($this->options->ICPbeian): ?>
-                <p><?php $this->options->ICPbeian(); ?></p>
+                <br><a href="http://beian.miit.gov.cn" target="_blank"><?php $this->options->ICPbeian(); ?></a>
                 <?php endif; ?>
             </div>
         </div>
