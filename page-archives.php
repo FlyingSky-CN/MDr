191781373c87a811522f398b209ae087809ff452
FlyingSky <me@fsky7.com>
2019-10-18 19:49:01 +0800

    Update page-archives.php

--- page-archives.php
+++ page-archives.php
@@ -9,35 +9,40 @@
 <?php $this->need('header.php'); ?>
 <div id="main">
 <?php if (!empty($this->options->Breadcrumbs) && in_array('Pageshow', $this->options->Breadcrumbs)): ?>
-<div class="breadcrumbs">
-<a href="<?php $this->options->siteUrl(); ?>">首页</a> &raquo; <?php $this->title() ?>
+<div class="mdui-card" style="margin-top:20px;background-color: rgba(180, 180, 180, 0.25);">
+    <span class="mdui-chip-icon" style="border-radius:2px;"><i class="mdui-icon material-icons">chevron_right</i></span>
+    <span class="mdui-chip-title">
+        <a href="<?php $this->options->siteUrl(); ?>">首页</a> &raquo; <?php $this->title() ?>
+    </span>
 </div>
 <?php endif; ?>
-<article class="post">
-<h1 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
-<?php
-$stat = Typecho_Widget::widget('Widget_Stat');
-$this->widget('Widget_Contents_Post_Recent', 'pageSize='.$stat->publishedPostsNum)->to($archives);
-$year=0; $mon=0; $i=0; $j=0;
-$output = '<div id="archives" class="mdui-typo">';
-while($archives->next()){
-	$year_tmp = date('Y',$archives->created);
-	if ($year > $year_tmp) {
-		$output .= '</ul>';
-	}
-	if ($year != $year_tmp) {
-		$year = $year_tmp;
-		$output .= '<h3>'.date('Y 年',$archives->created).'</h3><ul>';
-	}
-	if ($this->options->PjaxOption && $archives->hidden) {
-		$output .= '<li>'.date('m/d：',$archives->created).'<a>'. $archives->title .'</a></li>';
-	} else {
-		$output .= '<li>'.date('m/d：',$archives->created).'<a href="'.$archives->permalink .'">'. $archives->title .'</a></li>';
-	}
-}
-$output .= '</ul></div>';
-echo $output;
-?>
-</article>
+<div class="mdui-card" style="margin-top:20px;">
+    <div class="mdui-card-primary">
+        <div class="mdui-card-primary-title"><?php $this->title() ?></div>
+        <div class="mdui-card-primary-subtitle">Archives</div>
+    </div>
+    <div class="mdui-card-content mdui-typo" style="padding: 0px 16px 16px 16px;">
+        <?php
+        $stat = Typecho_Widget::widget('Widget_Stat');
+        $this->widget('Widget_Contents_Post_Recent', 'pageSize='.$stat->publishedPostsNum)->to($archives);
+        $year=0; $mon=0; $i=0; $j=0;
+        $output = '<div class="mdui-list mdui-list-dense">';
+        while($archives->next()){
+        	$year_tmp = date('Y',$archives->created);
+        	if ($year != $year_tmp) {
+        		$year = $year_tmp;
+        		$output .= '<h4>'.date('Y 年',$archives->created).'</h4>';
+        	}
+        	if ($this->options->PjaxOption && $archives->hidden) {
+        		$output .= '<li class="mdui-list-item mdui-ripple"><div class="mdui-list-item-content">'.date('m/d：',$archives->created).'<a>'. $archives->title .'</a></div></li>';
+        	} else {
+        		$output .= '<li class="mdui-list-item mdui-ripple"><div class="mdui-list-item-content">'.date('m/d：',$archives->created).'<a href="'.$archives->permalink .'">'. $archives->title .'</a></div></li>';
+        	}
+        }
+        $output .= '</div>';
+        echo $output;
+        ?>
+    </div>
+</div>
 </div>
 <?php $this->need('footer.php'); ?>
