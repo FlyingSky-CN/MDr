8102bfe95fb4c6d6e3fd377eec20a43a8a347550
paizi <admin@blingwang.cn>
2019-10-28 20:51:17 +0800

    UI | 夜间模式Chrome顶栏自动变色
    Signed-off-by: paizi <admin@blingwang.cn>

--- footer.php
+++ footer.php
@@ -444,12 +444,16 @@ console.log("\n %c MDr By FlyingSky %c https://fsky7.com/ %c \n","color:#fff;bac
         $('body').addClass('mdui-theme-accent-<?php $this->options->mdrAccentD() ?>');
         $('body').removeClass('mdui-theme-accent-<?php $this->options->mdrAccent() ?>');
         $('.mdui-appbar').css('background-color','#212121');
+		var meta = document.getElementsByTagName('meta');
+		meta["theme-color"].setAttribute('content','#000000');
     }
     function offDarkMode() {
         $('body').removeClass('mdui-theme-layout-dark');
         $('body').addClass('mdui-theme-accent-<?php $this->options->mdrAccent() ?>');
         $('body').removeClass('mdui-theme-accent-<?php $this->options->mdrAccentD() ?>');
         $('.mdui-appbar').css('background-color','#ffffff');
+		var meta = document.getElementsByTagName('meta');
+		meta["theme-color"].setAttribute('content','<?php if($this->options->mdrChrome){echo $this->options->mdrChrome();} else {echo "#FFFFFF";} ?>');
     }
 </script>
 <script>
