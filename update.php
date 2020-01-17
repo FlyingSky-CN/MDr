eeea23140e0ccb0aee32f871b036633b3995e924
FlyingSky-CN <me@fsky7.com>
2020-01-17 10:25:07 +0800

    Future | 自动更新 #11

--- update.php
+++ update.php
@@ -1,6 +1,51 @@
 <?php
+/**
+ * MDr 主题更新程序
+ */
+
 header("Content-Type: text/plain; charset=UTF-8");
+
+/* 载入 Typecho */
+
+if (!defined('__DIR__')) {
+    define('__DIR__', dirname(__FILE__));
+}
+
+if (!defined('__TYPECHO_ROOT_DIR__') && !@include_once __DIR__ . '/../../../config.inc.php') {
+    file_exists(__DIR__ . '/../../../install.php') ? header('Location: ../../../install.php') : print('载入 Typecho 失败');
+    exit;
+}
+
+/* 注册 Widget */
+
+Typecho_Widget::widget('Widget_Options')->to($options);
+Typecho_Cookie::setPrefix($options->originalSiteUrl);
+Typecho_Widget::widget('Widget_User')->to($user);
+
+/* 验证 Administrator 权限 */
+
+if (!$user->hasLogin()) {
+    /* 未登录 */
+    header('HTTP/1.1 403 Forbidden');
+    header('Location: ../../../admin/login.php');
+    print('没有权限访问');
+    exit();
+}
+
+if (!$user->pass('administrator',true)) {
+    /* 不是 Administrator */
+    header('HTTP/1.1 403 Forbidden');
+    header('Location: ../../../admin/login.php');
+    print('没有权限访问');
+    exit();
+}
+
+/* 更新程序 */
+
+define('__MDR_RAW_URL__', 'https://raw.githubusercontent.com/FlyingSky-CN/MDr/1.0.3/');
+
 echo "MDr主题更新程序";
+
 function curl($url){
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
@@ -13,18 +58,30 @@ function curl($url){
     curl_close($curl);
     return $result;
 }
-$hash = curl('https://raw.githubusercontent.com/FlyingSky-CN/MDr/1.0.3/hash.txt');
+
+$hash = curl(__MDR_RAW_URL__ . 'hash.txt');
+
 echo "\n\n获取文件Hash...\n\n";
-echo $hash."\n\n";
+
+if (!$hash) {
+    /* Hash 获取失败 */
+    print('文件 Hash 表获取失败，请检查服务器与 ' . __MDR_RAW_URL__ . '的连通性。');
+    exit();
+}
+
+echo $hash . "\n\n";
+
 $hash = explode("\n", $hash);
 array_pop($hash);
+
 echo "检查本地文件...\n\n";
+
 foreach ($hash as $remote) {
     list($remote_sha256, $filename) = explode('  ', $remote);
     $trimname = trim($filename);
     if (!file_exists(__DIR__.'/'.$trimname) || !hash_equals(hash('sha256', file_get_contents(__DIR__.'/'.$trimname)), $remote_sha256)) {
         echo "检测到 ".$trimname." 有新版本";
-        $url = 'https://raw.githubusercontent.com/FlyingSky-CN/MDr/1.0.3/'.$trimname;
+        $url = __MDR_RAW_URL__ . $trimname;
         if (file_put_contents(__DIR__.'/'.$trimname,curl($url))) {
             echo "，已更新\n";
         } else {
@@ -34,6 +91,7 @@ foreach ($hash as $remote) {
         echo "Hash相同，无需更新  ".$trimname."\n";
     }
 }
-echo "\n\n任务完成";
-die();
-?>
+
+echo "\n任务完成";
+
+exit();?>
